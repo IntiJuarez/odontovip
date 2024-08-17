4ff4db6837194cb8b8ed3deccc04848ab403d776
Lucas Balderrama <lbalderrama@icebergsolutions.com>
2024-08-17 20:39:54 -0300

    fix slide

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
 
-    let slideIndex = 0;
-    const slides = document.querySelector('.slide');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    function showNextSlide() {
-        slideIndex++;
-        if (slideIndex === totalSlides) {
-            slideIndex = 0;
-        }
-        slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+function showNextSlide() {
+    slideIndex++;
+    if (slideIndex === totalSlides) {
+        slideIndex = 0;
     }
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
 
-    setInterval(showNextSlide,3000);
-
+    setInterval(showNextSlide,3000);    
+    
 });
 
 document.addEventListener('DOMContentLoaded', () => {

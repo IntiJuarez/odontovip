ef5539302d7f9d785837c924e85ba339414844e6
Lucas Balderrama <lbalderrama@icebergsolutions.com>
2024-08-17 21:10:08 -0300

    fix feature grid

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-let slideIndex = 0;
-const slides = document.querySelector('.slides');
-const totalSlides = document.querySelectorAll('.slide').length;
+    let slideIndex = 0;
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
 
-function showNextSlide() {
-    slideIndex++;
-    if (slideIndex === totalSlides) {
-        slideIndex = 0;
+    function showNextSlide() {
+        slideIndex++;
+        if (slideIndex === totalSlides) {
+            slideIndex = 0;
+        }
+        slides.style.transform = `translateX(-${slideIndex * 100}%)`;
     }
-    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
-}
 
-    setInterval(showNextSlide,3000);    
-    
+    setInterval(showNextSlide, 3000);
+
 });
 
 document.addEventListener('DOMContentLoaded', () => {

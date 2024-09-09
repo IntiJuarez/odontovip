29db935db3d0a76b524789724ad2055dbb446bd2
intil <intijuarez.prof@gmail.com>
2024-09-08 21:04:08 -0300

    Agregado de script y correciones en versión web

--- script.js
+++ script.js
@@ -32,3 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 60;
+            if(window.scrollY >= sectionTop) {
+                current = section.getAttribute('id')
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });
+    });
+});
+
+
